060d506841cb4101c54c6242a255b533a3f78b12
justinfineberg <justinf1818@gmail.com>
2021-09-30 12:44:13 -0400

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res)=>{
+    res.send('<h1>hello</h1>')
+})
+
+const port = process.env.PORT
+
+server.listen(port, ()=>{
+    console.log(`server up on ${port}`)
+})
